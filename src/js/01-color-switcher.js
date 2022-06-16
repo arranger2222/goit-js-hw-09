434b17a44b5fc4d84e6d76380cfc8031df985a3f
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2022-06-16 17:10:40 +0300

    finished the first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]')
+};
+refs.stopBtn.setAttribute('disabled', '');
+let changeColorTimer = null;
 
+
+refs.startBtn.addEventListener('click', startRandom);
+refs.stopBtn.addEventListener("click", () => {
+    clearInterval(changeColorTimer)
+    refs.startBtn.removeAttribute('disabled');
+    refs.stopBtn.setAttribute('disabled', '');    
+    }    
+    );
+
+function startRandom () {
+    changeColorTimer = setInterval(() =>{
+        document.body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+    refs.startBtn.setAttribute('disabled', '');
+    refs.stopBtn.removeAttribute('disabled');    
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
